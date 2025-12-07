72278ccfcb384e808719c0f18e09861a5261a809
Priyanshu <priyanshu@aereo.io>
2025-12-07 16:52:56 +0000

    wip

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from typing import Union
-
 from fastapi import FastAPI
+from langchain.chat_models import init_chat_model
 
 app = FastAPI()
 
+model = init_chat_model(
+    model="Qwen/Qwen3-4B-Instruct-2507",
+    model_provider="openai",
+    base_url="http://localhost:8001/",
+    api_key="YOUR_API_KEY",
+)
 
-@app.get("/")
+
+@app.post("/chat")
 def ping():
     return {"ping": "pong"}
