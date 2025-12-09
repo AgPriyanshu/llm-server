6f1767c35cfccefc122a382985f56736ade312d9
Priyanshu <priyanshu@aereo.io>
2025-12-09 18:15:49 +0000

    dockerized the application

---
+++ README.md
@@ -0,0 +1,6 @@
+**TODO
+- Create docker compose for this project
+- Add postgres db for chats
+- Add vector db for embeddits
+- Create websocket for chat
+- Enable model streaming
--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 from fastapi import FastAPI
-from langchain.chat_models import init_chat_model
+from langchain.agents import create_agent
+from langchain.messages import HumanMessage, SystemMessage
+from langchain.tools import tool
+from langchain_ollama import ChatOllama
+
+
+@tool
+def get_weather(location: str) -> str:
+    """Get weather information for a location."""
+    return f"Weather in {location}: Sunny, 72°F"
+
+
+model = ChatOllama(model="qwen3:4b-instruct")
+
+agent = create_agent(
+    model=model,
+    system_prompt="You are a helpful assistant and give precise answers. You don't give your thoughts just do what has been asked",
+    tools=[get_weather],
+)
 
 app = FastAPI()
 
-model = init_chat_model(
-    model="Qwen/Qwen3-4B-Instruct-2507",
-    model_provider="openai",
-    base_url="http://localhost:8001/",
-    api_key="YOUR_API_KEY",
-)
 
-
-@app.post("/chat")
+@app.get("/")
 def ping():
-    return {"ping": "pong"}
+    messages = [
+        SystemMessage(
+            content="You are a helpful assistant and give precise answers. You don't give your thoughts just do what has been asked"
+        ),
+        HumanMessage(content="what is the weather in India"),
+    ]
+
+    response = agent.invoke({"messages": messages})
+    return {"ai_response": response}
